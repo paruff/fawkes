fefb04cb30b44dbcf9a9882ee8c0c30011456f61
Phil Ruff <phil.ruff@gmail.com>
2018-11-16 18:53:54 +0000

    Update infra/infra-k8s-boot.sh

--- infra/infra-k8s-boot.sh
+++ infra/infra-k8s-boot.sh
@@ -10,3 +10,15 @@ aws eks create-cluster \
   --resources-vpc-config subnetIds=${EKS_SUBNET_IDS},securityGroupIds=${EKS_SECURITY_GROUPS} \
   --kubernetes-version 1.10
   
+
+  aws cloudformation create-stack \
+  --stack-name k8s-workshop-worker-nodes \
+  --template-url https://amazon-eks.s3-us-west-2.amazonaws.com/1.10.3/2018-06-05/amazon-eks-nodegroup.yaml \
+  --capabilities "CAPABILITY_IAM" \
+  --parameters "[{\"ParameterKey\": \"KeyName\", \"ParameterValue\": \"${AWS_STACK_NAME}\"},
+                 {\"ParameterKey\": \"NodeImageId\", \"ParameterValue\": \"${EKS_WORKER_AMI}\"},
+                 {\"ParameterKey\": \"ClusterName\", \"ParameterValue\": \"k8s-workshop\"},
+                 {\"ParameterKey\": \"NodeGroupName\", \"ParameterValue\": \"k8s-workshop-nodegroup\"},
+                 {\"ParameterKey\": \"ClusterControlPlaneSecurityGroup\", \"ParameterValue\": \"${EKS_SECURITY_GROUPS}\"},
+                 {\"ParameterKey\": \"VpcId\", \"ParameterValue\": \"${EKS_VPC_ID}\"},
+                 {\"ParameterKey\": \"Subnets\", \"ParameterValue\": \"${EKS_SUBNET_IDS}\"}]"
