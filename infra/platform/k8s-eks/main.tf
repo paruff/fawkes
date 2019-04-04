71c49077e21314421757ccea7c7b3ce7871f4eca
Phil Ruff <phil.ruff@gmail.com>
2019-04-04 13:39:02 -0400

    increasing the size of the worker nodes/instances

--- infra/platform/k8s-eks/main.tf
+++ infra/platform/k8s-eks/main.tf
@@ -71,7 +71,7 @@ locals {
   worker_groups_launch_template = [
     {
       # This will launch an autoscaling group with only Spot Fleet instances
-      instance_type                            = "t2.small"
+      instance_type                            = "r5.large"
       additional_userdata                      = "echo foo bar"
       subnets                                  = "${join(",", module.vpc.private_subnets)}"
       additional_security_group_ids            = "${aws_security_group.worker_group_mgmt_one.id},${aws_security_group.worker_group_mgmt_two.id}"
