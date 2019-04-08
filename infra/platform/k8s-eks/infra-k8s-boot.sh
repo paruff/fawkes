bc30c826f32943596cd47dfecd05cb4a93e233a7
Phil Ruff <phil.ruff@gmail.com>
2019-04-08 17:25:09 -0400

    adding wait to most helm install commands

--- infra/platform/k8s-eks/infra-k8s-boot.sh
+++ infra/platform/k8s-eks/infra-k8s-boot.sh
@@ -23,34 +23,36 @@ kubectl -n kube-system create serviceaccount tiller
 kubectl create clusterrolebinding tiller --clusterrole cluster-admin --serviceaccount=kube-system:tiller
 helm init --service-account tiller 
 # --tiller-tls-verify
+kubectl rollout status -h
 
-helm install stable/kubernetes-dashboard --name dashboard-demo
+helm install --wait stable/kubernetes-dashboard --name dashboard-demo
 
 
 # Helm up basic
 # kubectl create namespace pipeline
 # # Jenkins
-helm install --namespace=pipeline stable/jenkins --name jenkins
+helm install --namespace=pipeline stable/jenkins --name jenkins --wait
 # # kubectl get svc --namespace pipeline -w jenkins
 # # capture url and admin password
 ##
 # printf $(kubectl get secret --namespace pipeline jenkins -o jsonpath="{.data.jenkins-admin-password}" | base64 --decode);echo
 # 
-# export SERVICE_IP=$(kubectl get svc --namespace pipeline jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
+# export JENKINS_SERVICE_IP=$(kubectl get svc --namespace pipeline jenkins --template "{{ range (index .status.loadBalancer.ingress 0) }}{{ . }}{{ end }}")
 #  echo http://$SERVICE_IP:8080/login
 # # list plugins
 
 
 
-helm install --namespace=pipeline stable/sonarqube --name sonarqube
+helm install --namespace=pipeline stable/sonarqube --name sonarqube --wait
 # # Configure auth, uid: admin, pw:admin
-# export SERVICE_IP=$(kubectl get svc --namespace pipeline sonarqube-sonarqube -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
+# export SONAR_SERVICE_IP=$(kubectl get svc --namespace pipeline sonarqube-sonarqube -o jsonpath='{.status.loadBalancer.ingress[0].ip}')
 # echo http://$SERVICE_IP:9000
 # # add plugins
 
-helm install --namespace=pipeline stable/sonatype-nexus --name nexus
-## where is the url
+helm install --namespace=pipeline stable/sonatype-nexus --name nexus --set nexus.service.type=LoadBalancer --wait
+## where is the url? change nexus.service.type to loadbalancer --set nexus.service.type=LoadBalancer
 ## uid: admin, pw: admin123
+## I don't seem to have access externally
 
 
 helm install --namespace=pipeline stable/selenium --name selenium
@@ -58,14 +60,15 @@ helm install --namespace=pipeline stable/selenium --name selenium
 
 # helm install --namespace=pipeline stable/spinnaker --name spinnaker
 # # Satisfied
-helm install --namespace=pipeline stable/prometheus --name prometheus
+helm install --namespace=pipeline stable/prometheus --name prometheus --wait
 
-helm install --namespace=pipeline stable/elastic-stack --name elk
+helm install --namespace=pipeline stable/elastic-stack --name elk --wait
 
 # helm install --namespace=pipeline stable/anchore-engine --name anchore
 
 # helm install --namespace=pipeline stable/ --name 
 
+helm install --namespace=pipeline --name jmeter stable/distributed-jmeter --wait
 
 # # Delight
 
