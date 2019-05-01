ab41a53f6e8be7aade596ac30a3bec554c3acde7
Shawn Sheldon <michael.sheldon@unisys.com>
2019-04-30 21:40:17 -0500

    Prometheous rock'n and roll'n

--- infra/platform/k8s-eks/prometheus/prometheus-reinstall.sh
+++ infra/platform/k8s-eks/prometheus/prometheus-reinstall.sh
@@ -3,20 +3,35 @@
 ## Use to update Prometheus with new configurations (from prometheus-values.yaml)
 ## Note: sometimes it is necessary to rerun/retry this script multiple times as some resources may not get deleted quickly enough
 
-helm ls --all prom
-helm del prom
-helm del --purge prom
-kubectl delete pvc -l release=prom,component=data
+# BEGIN Cleanup
+for n in $(kubectl get namespaces -o jsonpath={..metadata.name}); do
+  kubectl delete --all --namespace=pipeline prometheus,servicemonitor,alertmanager
+done
 
-helm install --name prom --namespace pipeline -f prometheus-values.yaml  stable/prometheus-operator
+sleep 120
+
+kubectl delete -f bundle.yaml
+
+for n in $(kubectl get namespaces -o jsonpath={..metadata.name}); do
+  kubectl delete --ignore-not-found --namespace=pipeline service prometheus-operated alertmanager-operated
+done
+
+kubectl delete --ignore-not-found customresourcedefinitions \
+  prometheuses.monitoring.coreos.com \
+  servicemonitors.monitoring.coreos.com \
+  alertmanagers.monitoring.coreos.com \
+  prometheusrules.monitoring.coreos.com
+# END Cleanup
+
+kubectl create secret generic additional-scrape-configs --from-file=prometheus-additional.yaml --dry-run -oyaml > additional-scrape-configs.yaml
+helm install --name prometheus --namespace pipeline -f prometheus-values.yaml stable/prometheus-operator
+# helm install --namespace=pipeline -f prometheus-values.yaml stable/prometheus --name prometheus --wait
 
 kubectl get crd
 kubectl get pods -n pipeline
 
-# Alert manager
-kubectl port-forward -n pipeline alertmanager-prom-prometheus-operator-alertmanager-0 9093
-kubectl port-forward -n pipeline prometheus-alertmanager-7f67c49686-qvx4z 9093
+# Alert manager port-forward example
+# kubectl port-forward -n pipeline prometheus-alertmanager-7f67c49686-qvx4z 9093
 
-# Prometheus server
-kubectl port-forward -n pipeline prometheus-prom-prometheus-operator-prometheus-0 9090
-kubectl port-forward -n pipeline prometheus-server-75679ccf4f-7fr6c 9090
+# Prometheus server port-forward example
+# kubectl port-forward -n pipeline prometheus-server-75679ccf4f-7fr6c 9090
