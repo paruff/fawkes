e2517fcebdb0bdcfbf00ecc19e084e53ad7e557c
Shawn Sheldon <michael.sheldon@unisys.com>
2019-05-14 08:46:22 -0500

    use pipeline namespace for kygieia

--- infra/platform/k8s-eks/hygieia/hygieia-reinstall.sh
+++ infra/platform/k8s-eks/hygieia/hygieia-reinstall.sh
@@ -3,46 +3,49 @@
 # Imperative install/reinstall of Hygieia
 
 # BEGIN Cleanup
-kubectl delete svc ui
-kubectl delete deployment ui
+kubectl --namespace pipeline delete svc ui
+kubectl --namespace pipeline delete deployment ui
 
-kubectl delete svc api
-kubectl delete deployment api
+kubectl --namespace pipeline delete svc api
+kubectl --namespace pipeline delete deployment api
 
-kubectl delete svc db
-kubectl delete deployment db
+kubectl --namespace pipeline delete svc db
+kubectl --namespace pipeline delete deployment db
 
-kubectl delete configmap mongo-initdb
-kubectl delete pvc db-data
+kubectl --namespace pipeline delete configmap mongo-initdb
+kubectl --namespace pipeline delete pvc db-data
 
 sleep 20
 
 # END Cleanup
 
-kubectl create -f db-data-persistentvolumeclaim.yaml
+# TODO: remove this line when calling this script from infra-k8s-boot.sh
+kubectl create namespace pipeline
 
-kubectl create configmap mongo-initdb --from-file=createuser.sh
+kubectl create --namespace pipeline -f db-data-persistentvolumeclaim.yaml
 
-kubectl create -f db-deployment.yaml
-# kubectl logs -f deploy/db
+kubectl create --namespace pipeline configmap mongo-initdb --from-file=createuser.sh
+
+kubectl create --namespace pipeline -f db-deployment.yaml
+# kubectl --namespace pipeline logs -f deploy/db
 # connect to a pod via bash
-# kubectl get pods --namespace pipeline (or leave off namespace when working with minikube)
+# kubectl get pods --namespace pipeline
 # kubectl -n pipeline exec -it ##POD_NAME## bash
 
-kubectl create -f db-service.yaml
-# kubectl port-forward svc/db 27017:27017
+kubectl create --namespace pipeline -f db-service.yaml
+# kubectl port-forward --namespace pipeline svc/db 27017:27017
 
 # wait a bit here until mongo is up and initial user is created
 sleep 30
 
-kubectl create -f api-deployment.yaml
-# kubectl logs -f deploy/api
-kubectl create -f api-service.yaml
+kubectl create --namespace pipeline -f api-deployment.yaml
+# kubectl --namespace pipeline logs -f deploy/api
+kubectl create --namespace pipeline -f api-service.yaml
 
 # wait a bit here until initial mongo collections are created by the API
 sleep 30
 
-kubectl create -f ui-deployment.yaml
-# kubectl logs -f deploy/ui
-kubectl create -f ui-service.yaml
-# kubectl port-forward svc/ui 3000:3000
+kubectl create --namespace pipeline -f ui-deployment.yaml
+# kubectl --namespace pipeline logs -f deploy/ui
+kubectl create --namespace pipeline -f ui-service.yaml
+# kubectl port-forward --namespace pipeline svc/ui 3000:3000
