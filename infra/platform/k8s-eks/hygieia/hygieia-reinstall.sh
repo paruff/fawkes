626dac6af0cf398eebd7f2c6361c5e928cd76a15
Shawn Sheldon <michael.sheldon@unisys.com>
2019-05-17 09:02:56 -0500

    add more jenkins collectors for later use

--- infra/platform/k8s-eks/hygieia/hygieia-reinstall.sh
+++ infra/platform/k8s-eks/hygieia/hygieia-reinstall.sh
@@ -3,6 +3,8 @@
 # Imperative install/reinstall of Hygieia
 
 # BEGIN Cleanup
+# kubectl --namespace pipeline delete deployment jenkins-cucumber-test-collector
+# kubectl --namespace pipeline delete deployment jenkins-codequality-collector
 kubectl --namespace pipeline delete deployment jenkins-build-collector
 
 kubectl --namespace pipeline delete svc ui
@@ -53,5 +55,6 @@ kubectl create --namespace pipeline -f ui-service.yaml
 # kubectl port-forward --namespace pipeline svc/ui 3000:3000
 
 # Add Collectors
-
 kubectl create --namespace pipeline -f jenkins-build-collector-deployment.yaml
+# kubectl create --namespace pipeline -f jenkins-codequality-collector-deployment.yaml
+# kubectl create --namespace pipeline -f jenkins-cucumber-test-collector-deployment.yaml
