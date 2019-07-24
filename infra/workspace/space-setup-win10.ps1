56b0423688b82e03ef4fb3e1a38c8e1716cc326c
Phil Ruff <phil.ruff@gmail.com>
2019-07-24 15:17:01 -0400

    working toward a ps version of the space-setup

--- infra/workspace/space-setup-win10.ps1
+++ infra/workspace/space-setup-win10.ps1
@@ -1,35 +1,57 @@
-:: Chocolatey install script from earlier
+#Requires -RunAsAdministrator
 
-if(test-path "C:\ProgramData\chocolatey\choco.exe"){
-
-@powershell -NoProfile -ExecutionPolicy Bypass -Command "iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))" && SET "PATH=%PATH%;%ALLUSERSPROFILE%\chocolatey\bin"
-
+$ChocoInstalled = $false
+if (Get-Command choco.exe -ErrorAction SilentlyContinue) {
+    $ChocoInstalled = $true
+}
+else {
+    Write-Host "chocolatey does not exist"
+    Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
 }
 
-:: Install all the packages
-:: -y confirm yes for any prompt during the install process ﻿
+choco list -local-only > choco-versions.txt
 
-choco install -y adoptopenjdk --version 8.192
-choco install -y awscli --version 1.16.198
-choco install -y git --version 2.22.0 # git 
-choco install -y gitflow-avh --version 0.0.0 # branching methodology last updated 2015 ?
-choco install -y docker-cli --version 18.09.6 # docker-toolbox ?
-choco install -y docker-compose --version 1.24.0
-choco install -y docker-machine --version 0.16.1
-choco install -y helm --version 2.14.2
-choco install -y kubernetes-cli --version 1.15.0
-choco install -y minikube --version 1.2.0 
-choco install -y maven --version 3.6.1
-choco install -y nodejs-lts --version 10.16.0 # Node.js LTS, Recommended for most users
-choco install -y postman --version 7.2.2
-choco install -y springtoolsuite --version 3.9.6
-choco install -y terraform --version 0.12.3
-choco install -y vagrant --version 2.2.5
-choco install -y virtualbox --version 6.0.10
-choco install -y vscode --version 1.36.1
 
-:: choco install <package_name> repeats for all the packages you want to install
-﻿
-:: foreach($PackageName in Get-Content .\tool-suite.txt) {
-::     choco install $PackageName -y
-:: }
+InstallIf adoptopenjdk8 , 8.212.2
+#InstallIf awscli , 1.16.200
+#InstallIf chefdk , 4.1.7
+#InstallIf docker-cli , 18.09.6
+#InstallIf docker-compose , 1.24.0
+#InstallIf docker-machine , 0.16.1
+#InstallIf git , 2.22.0 
+#InstallIf gitflow-avh , 0.0.0
+#InstallIf googlechrome , 75.0.3770.142
+#InstallIf inspec ,4.7.18
+#InstallIf kubernetes-cli , 1.15.1
+#InstallIf kubernetes-helm , 2.14.2
+#InstallIf make , 4.2.1
+#InstallIf maven , 3.6.1
+#InstallIf minikube , 1.2.0 
+#InstallIf mobaxterm , 11.1.0
+## nodejs lts is 10.16.0
+#InstallIf nodejs , 10.16.0
+## Call ?:InstallIf openjdk , 11.0.2.01
+#InstallIf postman , 7.2.2
+#InstallIf selenium-chrome-driver , 75.0.3770.8
+#InstallIf springtoolsuite , 3.9.6
+#InstallIf terraform , 0.12.5
+#InstallIf vagrant , 2.2.5
+#InstallIf virtualbox , 6.0.10
+#InstallIf vscode , 1.36.1
+
+
+function InstallIf {
+
+Param ([String]$package,[String]$version)
+
+if (Select-String -Path choco-versions.txt -Pattern "$package" -Quiet)
+{
+    Write-Output Found $package $version
+}
+else
+{
+Write-Output installing $package $version
+choco upgrade "$package"  -y --version "$version"
+Write-Output intalled $package $version
+}
+}
