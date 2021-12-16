66d36f535561bc581e94f30670ad250d9710a7ed
Phil Ruff <philip.ruff@phreesia.com>
2021-12-16 12:38:20 -0500

    updating tool versions

--- infra/workspace/space-setup-win10.bat
+++ infra/workspace/space-setup-win10.bat
@@ -28,26 +28,27 @@ REM hard to coordinate and little value with other platforms
 REM for /f "tokens=1-2 delims=," %%i in (tool-suite.txt) do (
 REM  Call :InstallIf %%i , %%j
 REM )
-Call :InstallIf adoptopenjdk8 , 8.212.2
+:: Call :InstallIf adoptopenjdk8 , 8.212.2
 Call :InstallIf awscli , 2.4.6
 Call :InstallIf aws-iam-authenticator ,  0.5.3
-Call :InstallIf chefdk , 4.1.7
+Call :InstallIf chef-workstation , 21.11.679
 Call :InstallIf docker-cli , 19.03.12
 Call :InstallIf docker-compose , 1.29.2
 Call :InstallIf docker-machine , 0.16.2
 Call :InstallIf git , 2.34.1
-Call :InstallIf gitflow-avh , 0.0.0
+:: Call :InstallIf gitflow-avh , 0.0.0
 Call :InstallIf googlechrome , 96.0.4664.110
-Call :InstallIf inspec , 4.46.13
+:: Call :InstallIf inspec , 4.46.13 included in chef workstation
 Call :InstallIf kubernetes-cli , 1.23.0
 Call :InstallIf kubernetes-helm , 3.7.1
 Call :InstallIf make , 4.3
 Call :InstallIf maven , 3.8.4
-Call :InstallIf minikube , 1.2.0 
+Call :InstallIf minikube , 1.24.0 
 Call :InstallIf microsoft-windows-terminal , 1.11.3471.0
 :: nodejs lts is 16.13.0
 Call :InstallIf nodejs , 16.13.0
-:: Call ?:InstallIf openjdk , 11.0.2.01
+:: LTS 8, 11, 17
+Call :InstallIf openjdk17 , 17.0.1
 Call :InstallIf postman , 9.4.1
 Call :InstallIf selenium-chrome-driver , 83.0.4103.39
 Call :InstallIf springtoolsuite , 3.9.6
