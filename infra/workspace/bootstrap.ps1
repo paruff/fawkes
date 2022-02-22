71c8f28eb4a23e67157ddaf6257c68d260471447
Phil Ruff <phil.ruff@gmail.com>
2022-02-22 12:10:41 -0500

    Update bootstrap.ps1
    Moving toward a PS script to install choco and the delivery tool suite

--- infra/workspace/bootstrap.ps1
+++ infra/workspace/bootstrap.ps1
@@ -1,51 +1,43 @@
-install-module cChoco
+function Install-ChocolateyPackage {
+  param (
+    [Parameter(Mandatory, Position=0)]
+    [string]$PackageName,
 
-Set-WSManInstance -ValueSet @{MaxEnvelopeSizekb "10000"} -ResourceURI winrm/config
+    [string]$Source,
 
-Start-DscConfiguration .\fawkesChocoConfig -wait -Verbose -force 
+    [alias("Params")]
+    [string]$PackageParameters,
 
+    [string]$Version,
 
-# function Install-ChocolateyPackage {
-#     param (
-#       [Parameter(Mandatory, Position=0)]
-#       [string]$PackageName,
-  
-#       [string]$Source,
-  
-#       [alias("Params")]
-#       [string]$PackageParameters,
-  
-#       [string]$Version,
-  
-#       [alias("Pre")]
-#       [switch]$Prerelease,
-  
-#       [switch]$UseInstallNotUpgrade
-#     )
-  
-#     $chocoExecutionArgs = "choco.exe"
-#     if ($UseInstallNotUpgrade) {
-#       $chocoExecutionArgs += " install"
-#     } else {
-#       $chocoExecutionArgs += " upgrade"
-#     }
-  
-#     $chocoExecutionArgs += " $PackageName -y"
-#     if ($Prerelease) { $chocoExecutionArgs += " --prerelease"}
-#     if ($Version) { $chocoExecutionArgs += " --version='$Version'"}
-#     if ($PackageParameters -and $PackageParameters -ne '') { $chocoExecutionArgs += " --package-parameters='$PackageParameters'"}
-  
-#     Invoke-Expression -Command $chocoExecutionArgs
-#     $exitCode = $LASTEXITCODE
-#     $validExitCodes = @(0, 1605, 1614, 1641, 3010)
-#     if ($validExitCodes -notcontains $exitCode) {
-#       throw "Error with package installation. See above."
-#     }
-#   }
-  
-#   Install-ChocolateyPackage gcloudsdk  -Version 0.0.0.20210904
-#   Install-ChocolateyPackage ojdkbuild   -Version 17.0.1.0
-#   Install-ChocolateyPackage vscode   -Version 1.64.2
-#   Install-ChocolateyPackage docker-desktop   -Version 4.5.0
-#   Install-ChocolateyPackage docker-machine   -Version 0.16.2
-  
+    [alias("Pre")]
+    [switch]$Prerelease,
+
+    [switch]$UseInstallNotUpgrade
+  )
+
+  $chocoExecutionArgs = "choco.exe"
+  if ($UseInstallNotUpgrade) {
+    $chocoExecutionArgs += " install"
+  } else {
+    $chocoExecutionArgs += " upgrade"
+  }
+
+  $chocoExecutionArgs += " $PackageName -y --source='$Source'"
+  if ($Prerelease) { $chocoExecutionArgs += " --prerelease"}
+  if ($Version) { $chocoExecutionArgs += " --version='$Version'"}
+  if ($PackageParameters -and $PackageParameters -ne '') { $chocoExecutionArgs += " --package-parameters='$PackageParameters'"}
+
+  Invoke-Expression -Command $chocoExecutionArgs
+  $exitCode = $LASTEXITCODE
+  $validExitCodes = @(0, 1605, 1614, 1641, 3010)
+  if ($validExitCodes -notcontains $exitCode) {
+    throw "Error with package installation. See above."
+  }
+}
+
+Install-ChocolateyPackage gcloudsdk -Source https://internal/repo/v2 -Version 0.0.0.20210904
+Install-ChocolateyPackage ojdkbuild -Source https://internal/repo/v2 -Version 17.0.1.0
+Install-ChocolateyPackage vscode -Source https://internal/repo/v2 -Version 1.64.2
+Install-ChocolateyPackage docker-desktop -Source https://internal/repo/v2 -Version 4.5.0
+Install-ChocolateyPackage docker-machine -Source https://internal/repo/v2 -Version 0.16.2
