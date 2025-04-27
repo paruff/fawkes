1ee9022f14199e4cae6ecfa3a448fb541b052cd5
Phil Ruff <phil.ruff@gmail.com>
2025-04-27 19:57:27 +0100

    updated helm chart versions

--- infra/platform/buildplatform.sh
+++ infra/platform/buildplatform.sh
@@ -16,52 +16,52 @@ if ! kubectl get namespace "$NAMESPACE" &>/dev/null; then
   kubectl create namespace "$NAMESPACE"
 fi
 
-# Add and update Helm repos
-helm repo add k8s-dashboard https://kubernetes.github.io/dashboard
-helm repo add jenkinsci https://charts.jenkins.io/
-helm repo add sonarqube https://SonarSource.github.io/helm-chart-sonarqube
-helm repo add harbor https://helm.goharbor.io
-helm repo add bitnami https://charts.bitnami.com/bitnami
-helm repo add douban https://douban.github.io/charts/
-helm repo add opsmx https://helmcharts.opsmx.com/
-helm repo add prometheus-community https://prometheus-community.github.io/helm-charts
-helm repo add elastic https://helm.elastic.co
-helm repo add anchore-charts https://charts.anchore.io
-helm repo add cloudnativeapp https://cloudnativeapp.github.io/charts/curated/
+# Function to add a Helm repo only if it doesn't already exist
+add_helm_repo() {
+  local name="$1"
+  local url="$2"
+  if ! helm repo list | grep -q "^$name"; then
+    helm repo add "$name" "$url"
+  fi
+}
+
+# Add and update Helm repos (no deprecated 'stable' repo)
+add_helm_repo k8s-dashboard https://kubernetes.github.io/dashboard
+add_helm_repo jenkinsci https://charts.jenkins.io/
+add_helm_repo sonarqube https://SonarSource.github.io/helm-chart-sonarqube
+add_helm_repo harbor https://helm.goharbor.io
+add_helm_repo bitnami https://charts.bitnami.com/bitnami
+add_helm_repo douban https://douban.github.io/charts/
+add_helm_repo opsmx https://helmcharts.opsmx.com/
+add_helm_repo prometheus-community https://prometheus-community.github.io/helm-charts
+add_helm_repo elastic https://helm.elastic.co
+add_helm_repo anchore-charts https://charts.anchore.io
+add_helm_repo cloudnativeapp https://cloudnativeapp.github.io/charts/curated/
 helm repo update
 
-# Kubernetes Dashboard
-helm upgrade --install fawkes-k8s-dashboard k8s-dashboard/kubernetes-dashboard --namespace "$NAMESPACE" --version 5.0.5 --wait
+# Deploy platform components
+deploy_chart() {
+  local release="$1"
+  local chart="$2"
+  shift 2
+  echo "Deploying $release from $chart..."
+  if ! helm upgrade --install "$release" "$chart" --namespace "$NAMESPACE" --wait "$@"; then
+    echo "Error: Failed to deploy $release"
+    exit 1
+  fi
+}
 
-# Jenkins
-helm upgrade --install fawkes-jenkins jenkinsci/jenkins --namespace "$NAMESPACE" --values jenkins/values.yaml --wait
-
-# Sonarqube
-helm upgrade --install sonarqube sonarqube/sonarqube-lts --namespace "$NAMESPACE" --wait
-
-# Harbor
-helm upgrade --install my-harbor harbor/harbor --namespace "$NAMESPACE" --version 1.8.1 --wait
-
-# Keycloak
-helm upgrade --install my-keycloak bitnami/keycloak --namespace "$NAMESPACE" --version 6.0.0 --wait
-
-# Selenium
-helm upgrade --install fawkes-selenium douban/selenium --namespace "$NAMESPACE" --version 1.3.0 --wait
-
-# Spinnaker
-helm upgrade --install fawkes-spinnaker opsmx/spinnaker --namespace "$NAMESPACE" --version 2.2.7 --wait
-
-# Prometheus
-helm upgrade --install fawkes-prometheus prometheus-community/prometheus --namespace "$NAMESPACE" --version 15.0.2 --wait
-
-# ECK Operator (Elastic)
-helm upgrade --install fawkes-eck-operator elastic/eck-operator --namespace "$NAMESPACE" --version 1.9.1 --wait
-
-# Anchore Engine
-helm upgrade --install my-anchore-engine anchore-charts/anchore-engine --namespace "$NAMESPACE" --version 1.16.0 --wait
-
-# Distributed JMeter
-helm upgrade --install fawkes-distributed-jmeter cloudnativeapp/distributed-jmeter --namespace "$NAMESPACE" --version 1.0.1 --wait
+deploy_chart fawkes-k8s-dashboard k8s-dashboard/kubernetes-dashboard --version 7.0.0
+deploy_chart fawkes-jenkins jenkinsci/jenkins --version 5.1.13 --values jenkins/values.yaml
+deploy_chart sonarqube sonarqube/sonarqube-lts --version 10.5.0
+deploy_chart my-harbor harbor/harbor --version 1.14.2
+deploy_chart my-keycloak bitnami/keycloak --version 21.2.2
+deploy_chart fawkes-selenium douban/selenium --version 1.3.0
+deploy_chart fawkes-spinnaker opsmx/spinnaker --version 2.2.7
+deploy_chart fawkes-prometheus prometheus-community/prometheus --version 56.0.0
+deploy_chart fawkes-eck-operator elastic/eck-operator --version 2.11.1
+deploy_chart my-anchore-engine anchore-charts/anchore-engine --version 1.21.0
+deploy_chart fawkes-distributed-jmeter cloudnativeapp/distributed-jmeter --version 1.0.1
 
 echo "All platform components have been deployed (or upgraded) in the '$NAMESPACE' namespace."
 
