91652757546042c02c40e382092ff9b8c4107506
Phil Ruff <phil.ruff@gmail.com>
2025-05-17 13:55:38 +0100

    Create main.tf

---
+++ infra/azure/main.tf
@@ -0,0 +1,25 @@
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "aks_rg" {
+  name     = "aks-resource-group"
+  location = "East US"
+}
+
+resource "azurerm_kubernetes_cluster" "aks" {
+  name                = "aks-cluster"
+  location            = azurerm_resource_group.aks_rg.location
+  resource_group_name = azurerm_resource_group.aks_rg.name
+  dns_prefix          = "aksdns"
+
+  default_node_pool {
+    name       = "default"
+    node_count = 2
+    vm_size    = "Standard_DS2_v2"
+  }
+
+  identity {
+    type = "SystemAssigned"
+  }
+}
