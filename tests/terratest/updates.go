dfe3ecfec4210eb02dec354fdc6636af2c08a3cb
Phil Ruff <phil.ruff@gmail.com>
2026-03-11 18:10:46 +0000

    Update Go dependencies in tests/terratest

---
+++ tests/terratest/updates.go
@@ -0,0 +1,9 @@
+# Updating Go dependencies
+
+# Update terratest library to specific version
+"""
+go get github.com/gruntwork-io/terratest@v0.47.2
+go mod tidy
+"""
+
+# Add any additional changes needed after updating.
